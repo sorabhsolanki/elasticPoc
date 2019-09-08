d946654ae3f7f32e335a22caf46311ca5ad8ef1c
sorabh solanki <sorabhs2009@gmail.com>
2019-09-08 12:02:50 +0530

    added initial structure for the code

---
+++ src/main/java/com/elastic/poc/elasticPoc/ElasticPocApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.poc.elasticPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticPocApplication.class, args);
+	}
+
+}
